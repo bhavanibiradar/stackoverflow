9711d7f5b37e05644108185b4152a1c257a25c8f
bhavanibiradar <bhavanirbiradar28@gmail.com>
2023-02-09 15:56:25 +0530

    init

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000'})
+const API = axios.create({ baseURL: 'https://stackoverflow-e7gd.onrender.com'})
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('Profile')){
